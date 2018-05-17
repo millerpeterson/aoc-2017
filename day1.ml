99ce0398370a82540068cba3746f1556d3a7982f
Miller Peterson <miller.peterson@gmail.com>
2018-05-16 23:20:57 -0400

    WIP beginning of a solution to day 1 part 2

--- day1.ml
+++ day1.ml
@@ -16,6 +16,18 @@ let consec_digit_sum digits_str =
   in
   consec_digit_sum' 0 wrapped_vals
 
+let rec halfway_consec_digit_sum' digits accum pos offset =
+  let list_len = List.length digits in
+  if (pos >= list_len) then accum
+  else
+    let halfway_offset_digit = List.nth digits ((pos + offset) % list_len) in
+    let curr_digit = List.nth digits pos in
+    match Option.both halfway_offset_digit curr_digit with
+    | Some (halfway_offset, curr) ->
+      let score = if halfway_offset = curr then curr else 0 in
+      halfway_consec_digit_sum' digits (accum + score) (pos + 1) offset
+    | None -> assert false
+
 let () =
   In_channel.read_all "captcha.txt"
   |> String.strip
