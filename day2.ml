a252ad32da612d57b3c65be08db1908d0fe56906
Miller Peterson <miller.peterson@gmail.com>
2021-07-29 11:48:41 -0400

    Part 2 of Day 2

--- day2.ml
+++ day2.ml
@@ -11,10 +11,13 @@ let line_to_row_values line =
   let space_split = String.split (String.strip line) ~on:'\t' in
   List.filter_map space_split ~f:int_of_string_opt
 
+let spreadsheet_vals spreadsheet_filename =
+  In_channel.read_all spreadsheet_filename
+  |> String.split_lines
+  |> List.map ~f:line_to_row_values
+
 let spreadsheet_checksum spreadsheet_filename =
-  let line_differences = In_channel.read_all spreadsheet_filename
-                         |> String.split_lines
-                         |> List.map ~f:line_to_row_values
+  let line_differences = spreadsheet_vals spreadsheet_filename
                          |> List.map ~f:row_difference
   in
   let checksum = List.reduce line_differences ~f:(+) in
@@ -24,3 +27,19 @@ let spreadsheet_checksum spreadsheet_filename =
 
 let () =
   assert (spreadsheet_checksum "spreadsheet.txt" = 39126)
+
+let ne_and_evenly_divides ab =
+  match ab with
+  | (a, b) -> Int.(<>) a b && Int.rem b a = 0
+
+let first_even_quotient row_values =
+  let pairs = List.cartesian_product row_values row_values in
+  let evenly_disible_pairs = List.filter pairs ~f:ne_and_evenly_divides in
+  match evenly_disible_pairs with
+  | [] -> 0
+  | (a, b)::_ -> b / a
+
+let spreadsheet_checksum2 spreadsheet_filename =
+   spreadsheet_vals spreadsheet_filename
+   |> List.map ~f:first_even_quotient
+   |> List.fold ~init:0 ~f:(+)
